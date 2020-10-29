84b37608269cd956eb36ce704bf6c047bd120ad1
Michael J. Beer <michael.beer@dlr.de>
2020-10-29 13:41:21 +0000

    Added configuration for bspwm

---
+++ polybar.config/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#------------------------------------------------------------------------------
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar mybar &
+
+echo "Polybar launched..."
+
+
