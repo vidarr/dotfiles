2a04fe29ed6ce313997d34a4b3918d46b55d531e
Michael J. Beer <michael.beer@dlr.de>
2021-01-09 12:12:03 +0000

    Single & Dual mon setup

--- polybar.config/launch.sh
+++ polybar.config/launch.sh
@@ -7,7 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
+polybar eDP-1 &
+polybar HDMI-2 &
 
 echo "Polybar launched..."
 
