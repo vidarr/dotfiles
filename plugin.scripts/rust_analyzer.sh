dc4b50a62912faecdd3c5113a4aab38041f45335
Michael J. Beer <michael.beer@dlr.de>
2021-02-11 09:35:17 +0000

    rust-analyzer for vim

---
+++ plugin.scripts/rust_analyzer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Install rust-analyzer
+#
+#------------------------------------------------------------------------------
+
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
+
