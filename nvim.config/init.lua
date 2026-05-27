edea58de8bc1320411530d4e16662f62c1de4ca5
Michael J. Beer <michael.beer@dlr.de>
2026-05-27 10:30:32 +0200

    Cursorline in nvim, highlight WARN and ERROR entries in log files

--- nvim.config/init.lua
+++ nvim.config/init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.cursorline = true
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.log",
+  callback = function()
+    vim.api.nvim_set_hl(0, "LogFileLineError", { bg = "#990000", fg = "#FFFF00", bold = true })
+    vim.api.nvim_set_hl(0, "LogFileLineWarn", { bg = "#FFFF00", fg = "#505050", bold = true })
+    vim.fn.matchadd("LogFileLineError", [=[.*ERROR.*]=])
+    vim.fn.matchadd("LogFileLineWarn", [=[.*WARN.*]=])
+  end,
+})
