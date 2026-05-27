1559ee10ef9ce0ea61ee03033ccaa577297b799a
Michael J. Beer <michael.beer@dlr.de>
2026-05-27 10:45:33 +0200

    Log file highlighting for DEBUG and INFO lines

--- nvim.config/init.lua
+++ nvim.config/init.lua
@@ -8,7 +8,11 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   callback = function()
     vim.api.nvim_set_hl(0, "LogFileLineError", { bg = "#990000", fg = "#FFFF00", bold = true })
     vim.api.nvim_set_hl(0, "LogFileLineWarn", { bg = "#FFFF00", fg = "#505050", bold = true })
+    vim.api.nvim_set_hl(0, "LogFileLineInfo", { bg = "#004999", fg = "#FFFFFF", bold = false })
+    vim.api.nvim_set_hl(0, "LogFileLineDebug", { bg = "#0080FF", fg = "#FFFFFF", bold = false })
     vim.fn.matchadd("LogFileLineError", [=[.*ERROR.*]=])
     vim.fn.matchadd("LogFileLineWarn", [=[.*WARN.*]=])
+    vim.fn.matchadd("LogFileLineInfo", [=[.*INFO.*]=])
+    vim.fn.matchadd("LogFileLineDebug", [=[.*DEBUG.*]=])
   end,
 })
